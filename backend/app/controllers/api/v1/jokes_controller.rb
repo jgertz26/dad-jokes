f6ee90e332ec23ec25fb14625cb749bf51d26a66
Jeremy <jgertz26@gmail.com>
2015-11-02 19:59:20 -0500

    index/show working with jsonapi_resources

--- backend/app/controllers/api/v1/jokes_controller.rb
+++ backend/app/controllers/api/v1/jokes_controller.rb
@@ -1,31 +1,3 @@
 class Api::V1::JokesController < ApplicationController
-  def index
-    @jokes = Joke.all
-    render json: {data: @jokes}
-  end
 
-  def show
-    @joke = Joke.find(params[:id])
-    render json: @joke
-  end
-
-  def create
-    binding.pry
-    joke = Joke.new(create_joke_params)
-
-    if joke.save
-      render json: joke,
-        status: :created,
-        location: [:api, :v1, joke]
-    else
-
-      render json: { errors: joke.errors }, status: :unprocessable_entity
-    end
-  end
-
-  private
-
-  def create_joke_params
-    params.require(:joke).permit(:setup, :punchline)
-  end
 end
